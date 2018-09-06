2c9e2f8d6657642dd4a05d635b22b287730aff5a
David VanScott <david.vanscott@gmail.com>
2018-09-06 09:58:51 -0400

    Update the title

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', "Leah & David's Wedding"),
 
     /*
     |--------------------------------------------------------------------------
